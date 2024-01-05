7373cb1ed00803e5c1c7365f9a995f2801493017
chinocarbon <1070980757@qq.com>
2024-01-05 18:38:11 +0800

    first commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from os import path
 import datetime as dt
 import json
-import time,random
+import time, random
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
